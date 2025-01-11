95149a14fe133c27bd9d4a3df5b480da64351e34
asnbby <1143318515@qq.com>
2025-01-11 17:18:14 +0800

    修改：     readme.md 	修改：     setup.sh

--- readme.md
+++ readme.md
@@ -4,7 +4,9 @@ Program Editor CHENYAN WU
 
 ## Setup
 - Clone repository 
-
+```
+git clone https://github.com/asnbby/InterviewSystem.git
+```
 - Setup conda environment
 ```
 conda create -n struct_llm python=3.9
@@ -14,8 +16,10 @@ conda activate struct_llm
 ```
 bash setup.sh
 ```
-- Download Embedding Model From Hugging-Face
-
+- Download Embedding Model From Hugging-Face(We use stella_en_400M_v5 as our embedding and retrieve model)
+```
+git lfs clone https://hf-mirror.com/dunzhang/stella_en_400M_v5
+```
 
 ## Run System
 ```
@@ -23,6 +27,7 @@ bash run.sh
 ```
 
 ## Acknowledgement
+```
 This project is intended for non-commercial use
 If you want to use it for commercial use, please contact wuchenyan0823@gmail.com
 ```
--- setup.sh
+++ setup.sh
@@ -1,9 +1,13 @@
-pip install torch==1.8.1+cu102 -f https://download.pytorch.org/whl/cu102/torch_stable.html
-pip install torch-scatter==2.0.6 -f https://data.pyg.org/whl/torch-1.8.1+cu102.html
-pip install torch-sparse==0.6.9 -f https://data.pyg.org/whl/torch-1.8.1+cu102.html
-pip install torch-cluster==1.5.9 -f https://data.pyg.org/whl/torch-1.8.1+cu102.html
-pip install torch-spline-conv==1.2.1 -f https://data.pyg.org/whl/torch-1.8.1+cu102.html
-pip install torch-geometric==1.7.0 -f https://data.pyg.org/whl/torch-1.8.1+cu102.html
-pip install transformers==2.2.0
-pip install nltk spacy sentencepiece numpy tqdm tensorboard
-python -m spacy download en
+#!/bin/bash
+
+# 检查是否存在 requirements.txt 文件
+if [ -f "requirements.txt" ]; then
+  echo "Found requirements.txt. Installing dependencies..."
+
+  # 安装 requirements.txt 中的依赖
+  pip install -r requirements.txt
+  
+  echo "Dependencies installed successfully."
+else
+  echo "requirements.txt not found in the current directory."
+fi
